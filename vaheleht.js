a58699858d29145eec17dfa1228336b8d3f1741a
tomlukk <tomtimolukk@gmail.com>
2024-12-02 21:03:06 +0200

    Vahelehe js viitamine

--- vaheleht.js
+++ vaheleht.js
@@ -1,15 +1,6 @@
-const navLinks = document.querySelectorAll('.navlink');
 
-// Get the current page's URL
-const currentPage = window.location.pathname;
-
-// Loop through the links and check if their href matches the current page
-navLinks.forEach(link => {
-    if (link.href.includes(currentPage)) {
-        link.classList.add('active');  // Add "active" class to the current page link
-    }
-});
-
+//Kood selles failis on tehisintellekti poolt genereeritud
+//Kood kerib alla kui vajutad keskmise teksti peale
 function scrollToSection() {
             const target = document.getElementById("scrollto");
             target.scrollIntoView({ behavior: "smooth" });
